e4782c001b20f8799a73f348d86caac1050bf187
Omoju <omojumiller@gmail.com>
2015-03-13 14:10:08 -0500

    Update to Data Exploration

--- bce-hiphop.sh
+++
@@ -1,6 +0,0 @@
-sudo apt-get purge python-nltk
-sudo apt-get install python-tk
-sudo pip2 install nltk plotly
-ipython2  -c 'import nltk; nltk.download("gutenberg")'
-curl -O https://omoju.github.io/JayZ.zip
-unzip -o JayZ.zip
